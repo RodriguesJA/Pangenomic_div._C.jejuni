2c88699a5f96ce68e1cbbdc1cf2a609472c4cec5
Jose Rodrigues, D.O., Ph.D <jrodrigues@albany.edu>
2023-09-29 12:47:51 -0400

    Update and rename fastqc_script.py to 1_fastqc_script.py

--- 1_fastqc_script.py
+++ 1_fastqc_script.py
@@ -3,7 +3,7 @@ import os  #This is the important to have biopython will have this already insta
 import urllib
 import re
 
-Genomes_file = open( "/mnt/gs18/scratch/groups/manninglab/cattlegenomes.txt" ) #Text file with all the genomes to be used in this pipeline. File is used instead of a directory in order to easily edit which genomes are utilized
+Genomes_file = open( "/[file path]/cattlegenomes.txt" ) #Text file with all the genomes to be used in this pipeline. File is used instead of a directory in order to easily edit which genomes are utilized
 Genomes_pair = Genomes_file.readlines() #Here you are reading all of the file names within a specific folder readlines enables this fx 
 
 for i in Genomes_pair: #Here we are stripping tabs and spaces from file names 
@@ -12,7 +12,8 @@ for i in Genomes_pair: #Here we are stripping tabs and spaces from file names
 	print (i)
 	pairGenome = i.split("\t")  
 	RefGenome = str(pairGenome[0])  #Here we place all of the integers within our file that we read and stripped into an array. In this case each TW# Will be in an array that is called RefGenome
-	os.system("fastqc ~/Campy_Genomes/" +RefGenome+ "/" +RefGenome+ "s1_*  ~/Campy_Genomes/" +RefGenome+ "/" +RefGenome+ "s2_* --outdir=/mnt/gs18/scratch/groups/manninglab/Cattle_All_QC_Results/fastqc/" + RefGenome)
+	os.system("fastqc ~/Campy_Genomes/" +RefGenome+ "/" +RefGenome+ "s1_*  ~/Campy_Genomes/" +RefGenome+ "/" +RefGenome+ "s2_* --outdir=/[filepath]fastqc/" + RefGenome)
 	print(RefGenome + "_DONE")
 #The above loop using the shell command to inact prokka while out putting the the files in the directory. Therefore each TW# will run through this loop and the last line prints which TW# is being utilized
-#Fastqc is the call and the commands. my file directory has the genome name / with s1_pe file names * means ignore the rest or all files with that s1_ in the name we provide the s1 and s2 and the outdirectory file   I put the output in files that have the name first and fastqc
+#Fastqc is the call and the commands. my file directory has the genome name / with s1_pe file names * means ignore the rest or all files with that s1_ in the name we provide the s1 and s2 and the outdirectory file
+# The first version of this script was created by Dr. Heather Blaneksnhip, PH.D.
